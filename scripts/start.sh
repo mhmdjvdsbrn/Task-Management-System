4c9c45337eb9e9f304ec030b615212af0906c078
mhmd javad sbrn <jvdsbrn@gmail.com>
2024-08-26 11:09:45 +0330

    fix in prod

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,8 +5,8 @@ echo "Apply database migrations"
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-echo "Run unit tests"
-python3 manage.py test task_backend.users.tests.test_model_users task_backend.users.tests.test_api_users task_backend.projects.tests.test_model_projects task_backend.projects.tests.test_api_projects --verbosity=2 --parallel
+# echo "Run unit tests"
+# python3 manage.py test task_backend.users.tests.test_model_users task_backend.users.tests.test_api_users task_backend.projects.tests.test_model_projects task_backend.projects.tests.test_api_projects --verbosity=2 --parallel
 
 # Start server
 echo "--> Starting web process"
